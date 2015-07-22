5227d7184df745ce15dd0ccac296e4c7c625b76a
Budi Oktaviyan Suryanto <budi.oktaviyan@icloud.com>
2015-07-22 23:04:07 +0700

    Refactoring

--- src/main/java/com/airsystem/sample/security/Application.java
+++ src/main/java/com/airsystem/sample/security/Application.java
@@ -1,8 +1,15 @@
 package com.airsystem.sample.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+
+import com.airsystem.sample.security.service.ApplicationService;
 
 /**
  * @author Budi Oktaviyan Suryanto (budi.oktaviyan@icloud.com)
@@ -10,7 +17,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class Application {
+public class Application extends GlobalAuthenticationConfigurerAdapter {
+
+	@Autowired
+	private ApplicationService applicationService;
+
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		ShaPasswordEncoder shaPasswordEncoder = new ShaPasswordEncoder();
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(applicationService);
+		authProvider.setPasswordEncoder(shaPasswordEncoder);
+		auth.authenticationProvider(authProvider);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
